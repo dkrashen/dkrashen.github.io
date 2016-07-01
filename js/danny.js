a374bd48a267ca676bcb4f7957a3b8fc075a3810
Danny <dkrashen@users.noreply.github.com>
2016-07-01 11:42:52 -0400

    spash is in!

--- js/danny.js
+++ js/danny.js
@@ -3,3 +3,13 @@ $(document).ready(function(){
 	var active_link_selector = "#".concat(active_link);
 	$(active_link_selector).addClass("active");
 });
+
+$(document).ready(function (){ 
+  $("#manuscript_clicker").click(function (){
+    $('html, body').animate({ 
+      scrollTop: $("#manuscripts").offset().top - 60,
+      easing: "jswing"}, 400);
+  });
+}); 
+  
+
